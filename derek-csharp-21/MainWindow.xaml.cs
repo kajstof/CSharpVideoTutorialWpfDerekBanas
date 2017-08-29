bc8d49841cecd6c385f0370a83adee24261184c9
Krzysztof Krysiak <krysiak86@gmail.com>
2017-08-29 14:24:38 +0200

    Add tutorial #20

---
+++ derek-csharp-21/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace derek_csharp_21
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.ShowDialog();
+        }
+
+        private void MenuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.ShowDialog();
+        }
+
+        private void MenuFontTimes_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontConsolas.IsChecked = false;
+            menuFontArial.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Times New Roman");
+        }
+
+        private void MenuFontConsolas_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontTimes.IsChecked = false;
+            menuFontArial.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Courier New");
+        }
+
+        private void MenuFontArial_Click(object sender, RoutedEventArgs e)
+        {
+            menuFontTimes.IsChecked = false;
+            menuFontConsolas.IsChecked = false;
+            txtBoxDoc.FontFamily = new FontFamily("Consolas");
+        }
+
+        private bool comboFSClosed = true;
+
+        private void ComboFontSize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboFSClosed)
+            {
+                ChangeTBFontSize();
+            }
+            comboFSClosed = true;
+        }
+
+        private void ComboFontSize_DropDownClosed(object sender, EventArgs e)
+        {
+            ComboBox combobox = sender as ComboBox;
+            comboFSClosed = !combobox.IsDropDownOpen;
+            ChangeTBFontSize();
+        }
+
+        private void ChangeTBFontSize()
+        {
+            string fontSize = comboFontSize.SelectedItem.ToString();
+            fontSize = fontSize.Substring(fontSize.Length - 2);
+            switch (fontSize)
+            {
+                case "10":
+                    txtBoxDoc.FontSize = 10;
+                    break;
+                case "12":
+                    txtBoxDoc.FontSize = 12;
+                    break;
+                case "14":
+                    txtBoxDoc.FontSize = 14;
+                    break;
+                case "16":
+                    txtBoxDoc.FontSize = 16;
+                    break;
+            }
+        }
+    }
+}
