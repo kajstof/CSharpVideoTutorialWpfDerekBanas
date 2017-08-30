f37df31cbba7a5f4786b06ddd126562fd6ff5cb9
Krzysztof Krysiak <krysiak86@gmail.com>
2017-08-30 13:12:02 +0200

    Add tutoriar #23

---
+++ derek-csharp-23/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace derek_csharp_23
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenerateDoc();
+        }
+
+        private void GenerateDoc()
+        {
+            FlowDocument doc = new FlowDocument();
+            Paragraph para = new Paragraph(new Run("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
+            doc.Blocks.Add(para);
+            para = new Paragraph(new Run("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))
+            {
+                FontSize = 14,
+                FontStyle = FontStyles.Italic,
+                Foreground = Brushes.Green
+            };
+            doc.Blocks.Add(para);
+            fdScrollViewer.Document = doc;
+        }
+
+        private void MyCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var calendar = sender as Calendar;
+            if (calendar.SelectedDate.HasValue)
+            {
+                DateTime date = calendar.SelectedDate.Value;
+                try
+                {
+                    tbDateSelected.Text = date.ToShortDateString();
+                }
+                catch (NullReferenceException) { }
+            }
+        }
+
+        private void TabItem_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ((int)e.Key >= 35 && (int)e.Key <= 68)
+            {
+                switch ((int)e.Key)
+                {
+                    case 35:
+                        strokeAttr.Width = 2;
+                        strokeAttr.Height = 2;
+                        break;
+                    case 36:
+                        strokeAttr.Width = 4;
+                        strokeAttr.Height = 4;
+                        break;
+                    case 37:
+                        strokeAttr.Width = 6;
+                        strokeAttr.Height = 6;
+                        break;
+                    case 38:
+                        strokeAttr.Width = 8;
+                        strokeAttr.Height = 8;
+                        break;
+                    case 45:
+                        strokeAttr.Color = (Color)ColorConverter.ConvertFromString("Blue");
+                        break;
+                    case 50:
+                        strokeAttr.Color = (Color)ColorConverter.ConvertFromString("Green");
+                        break;
+                    case 68:
+                        strokeAttr.Color = (Color)ColorConverter.ConvertFromString("Yellow");
+                        break;
+                }
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            var radiobutton = sender as RadioButton;
+            string radioBPressed = radiobutton.Content.ToString();
+            if (radioBPressed == "Draw")
+            {
+                this.DrawingCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            }
+            else if (radioBPressed == "Erase")
+            {
+                this.DrawingCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+            }
+            else if (radioBPressed == "Select")
+            {
+                this.DrawingCanvas.EditingMode = InkCanvasEditingMode.Select;
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("MyPicture.bin", FileMode.Create))
+            {
+                this.DrawingCanvas.Strokes.Save(fs);
+            }
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("MyPicture.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection sc = new StrokeCollection(fs);
+                this.DrawingCanvas.Strokes = sc;
+            }
+        }
+        private void RichTB_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            RichTextBox rtb = sender as RichTextBox;
+            if (rtb == null) return;
+            ContextMenu contextMenu = rtb.ContextMenu;
+            contextMenu.PlacementTarget = rtb;
+            contextMenu.Placement = PlacementMode.RelativePoint;
+            TextPointer position = rtb.Selection.End;
+            if (position == null) return;
+            Rect positionRect = position.GetCharacterRect(LogicalDirection.Forward);
+            contextMenu.HorizontalOffset = positionRect.X;
+            contextMenu.VerticalOffset = positionRect.Y;
+            contextMenu.IsOpen = true;
+            e.Handled = true;
+        }
+
+        private void SaveRTBContent(object sender, RoutedEventArgs e)
+        {
+            TextRange range = new TextRange(RichTB.Document.ContentStart, RichTB.Document.ContentEnd);
+            FileStream fStream = new FileStream(@"c:/Users/krysi/Desktop/C#Data/text.xaml", FileMode.Create);
+            range.Save(fStream, DataFormats.XamlPackage);
+            fStream.Close();
+        }
+
+        private void OpenRTBContent(object sender, RoutedEventArgs e)
+        {
+            TextRange range;
+            FileStream fStream;
+            if (File.Exists(@"c:/Users/krysi/Desktop/C#Data/text.xaml"))
+            {
+                range = new TextRange(RichTB.Document.ContentStart, RichTB.Document.ContentEnd);
+                fStream = new FileStream(@"c:/Users/krysi/Desktop/C#Data/text.xaml", FileMode.OpenOrCreate);
+                range.Load(fStream, DataFormats.XamlPackage);
+            }
+        }
+    }
+}
